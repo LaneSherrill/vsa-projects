4222369031efdcdcf2f5ebc1addebe08a43bd510
LaneSherrill <lbs.origami@gmail.com>
2017-06-20 10:18:01 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -1,22 +1,42 @@
-# Name:
-# Date:
+# Name:Lane Sherrill
+# Date:June 20, 2017
 
 # proj02_02: Fibonaci Sequence
 
+print """
+    Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
+    sequence is a sequence of numbers where the next number in the sequence is the sum of the 
+    previous two numbers in the sequence. The sequence looks like this: 
+    1, 1, 2, 3, 5, 8, 13...
 """
-Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
-sequence is a sequence of numbers where the next number in the sequence is the sum of the 
-previous two numbers in the sequence. The sequence looks like this: 
-1, 1, 2, 3, 5, 8, 13...
-"""
-
-print "!Fibonacci number generator!"
+print " "
 
 aon= raw_input("How many Fibonacci numbers do you want?: ")
 
-fnum1=1
+numc = 1
+nump = 0
+sum = numc + nump
+print sum
+for x in range(int(aon)-1):
+    sum = int(nump)+int(numc)
+    nump=numc
+    numc=sum
+    print sum
+
+numc=1
+nump=0
+sum= int(numc)+ int(nump)
+
+aon= raw_input("How many powers of two do you want?: ")
+
+for x in range(int(aon)+1):
+    sum= int(nump)+ int(numc)
+    numc=sum
+    nump=numc
+    if sum!= 1:
+        print sum
 
 
-for fnum1 in (0, int(aon)):
-    fnum1=1
+
+
 
