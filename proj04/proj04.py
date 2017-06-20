7a2513394abfc90d8c683236e80eec763db15af0
LaneSherrill <lbs.origami@gmail.com>
2017-06-20 15:25:51 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name:Lane Sherrill
+# Date: June 20, 2017
 
 """
 proj04
@@ -8,3 +8,39 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+pdrome= raw_input("Type what the word you want: ")
+
+plist=[]
+
+for letter in pdrome:
+    plist.append(letter)
+
+lplist= len(plist)
+
+for x in range(0,lplist):
+    wutfir= plist[1:]
+    wutlas= plist [:-1]
+    wutbot= plist[1:-1]
+    first= plist[0]
+    last= plist[(lplist-1)]
+
+
+
+
+
+
+    #TEST COMMANDS
+print ""
+print lplist
+print ""
+print plist
+print""
+print wutfir
+print""
+print wutlas
+print""
+print wutbot
+print""
+print first
+print ""
+print last
