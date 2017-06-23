13d57969a02dd059cc9aaacb276fb47f1f30c2f1
LaneSherrill <lbs.origami@gmail.com>
2017-06-23 15:37:45 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -50,3 +50,87 @@ if question1 == "yes":
 else:
     print 'Oh well...I didnt know how to code a loop anyways.'
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# FUN EXTRA THING IN DEV
+
+# Name:Lane Sherrill
+# Date:June 6, 2017
+
+# proj01: A Simple Program
+# This program asks the user for his/her name and age.
+# Then, it prints a sentence that says when the user will turn 100.
+
+# If you complete extensions, describe your extensions here!
+yes=0
+no=0
+repeatcount=0
+name= raw_input("Enter your name, first name only: ")
+
+# LANE NAME CODE
+if name=="Lane":
+    print
+    print "Shouldn't you already know what happens in this game experiment? You made it afterall."
+# SPENCER NAME CODE
+elif name=="Spencer":
+    print
+    print "That's the name of Lane's brother."
+    print
+    spencerconfirm= raw_input("Are you really Lane's brother?(y or n)")
+    while repeatcount<3:
+        while yes+no!=1:
+            if spencerconfirm=="y":
+                yes=yes+1
+            elif spencerconfirm=="n":
+                no=no+1
+            else:
+                spencerconfirm= raw_input("Your reply needs to be y or n: ")
+                repeatcount=repeatcount+1
+            if repeatcount == 3:
+                break
+    if repeatcount==3:
+        print "I'm tired of asking you..."
+# DAD NAME CODE
+elif name== "Karl":
+    print
+    print "Wow, your Lane's favorite dad!"
+    print
+# MOM NAME CODE
+elif name== "Melissa":
+    print
+    print "Wow, Lane's favorite mom is playing!"
+    print
+# MAGGIE NAME CODE
+elif name== "Maggie":
+    print "You're annoying"
+
+print
+print
+age= raw_input("Enter your age (a postive number, i.e. 3): ")
+
+
+
+
+
